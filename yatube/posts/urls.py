d8c5f39dc94a463c7f254ec28039998f55e74ecd
Anton Remizov <remizov.avas@gmail.com>
2023-03-07 16:01:02 +0400

    Add yatube project

---
+++ yatube/posts/urls.py
@@ -0,0 +1,14 @@
+# yatube/posts/urls.py (File of urls of the posts app)
+
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Home page
+    path('', views.index, name='home'),
+    # Groups pages
+    path('group/<slug:slug>/', views.group_posts, name='group_list')
+
+]
